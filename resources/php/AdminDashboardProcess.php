1e2306bfd20e6c5ea06d4e7826551ea980da0f2f
Atharva-Bhise <bhiseatharva18@gmail.com>
2024-12-23 20:24:59 +0530

    Added session_start() at starting

--- resources/php/AdminDashboardProcess.php
+++ resources/php/AdminDashboardProcess.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require 'C:/xampp/php/composer/vendor/autoload.php';
 
 // Specify the path to your .env file in the project root
@@ -8,7 +9,6 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header('Content-Type: application/json');
-session_start();
 if (!isset($_SESSION['login_status']) || $_SESSION['login_status'] === "loggedOff") {
     header("Location: ../html/adminLogin.html");
     exit;
